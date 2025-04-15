a4b54a6651bda69cf48102fc4e1408465b49a897
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-04-15 12:14:36 +0530

    feat: add Tailwind CSS configuration and update App component structure

--- TerraVista/src/App.jsx
+++ TerraVista/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import CountriesList from './components/CountriesList'
 
 const App = () => {
   return (
     <div>
       <div className="min-h-screen bg-gray-100 p-4">
         <h1 className="text-3xl font-bold text-center text-blue-500">REST Countries App</h1>
+        <p className="text-center text-gray-700 mt-2">Explore countries and their flags</p>
+        <CountriesList/>
       </div>
     </div>
   )
